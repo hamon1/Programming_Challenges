b002c8332390fcb5a9ee847d5120a1a58364113e
hamon1 <99328993+hamon1@users.noreply.github.com>
2023-01-02 17:37:37 +0900

    c3_try/c4_clear

--- Chapter01_basic/LCD_Display.java
+++ Chapter01_basic/LCD_Display.java
@@ -46,8 +46,159 @@
 
 package Chapter01_basic;
 
+import java.util.Scanner;
+
 public class LCD_Display {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            int s = sc.nextInt();
+            String n = sc.next();
+
+            if (s == 0 && n.equals("0")) break;
+
+            char[] number_list = new char[n.length()];
+            int len = n.length();
+
+            for (int i = 0; i < len; i++) {
+                number_list[i] = n.charAt(i);
+            }
+
+            for (int i = 0; i < len; i++) {
+                top(s, number_list[i]);
+                System.out.print(' ');
+            }
+            System.out.println();
+            for (int j = 0; j < s; j++) {
+                for (int i = 0; i < len; i++) {
+                    first(s, number_list[i]);
+                    System.out.print(' ');
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < len; i++) {
+                middle(s, number_list[i]);
+                System.out.print(' ');
+            }
+            System.out.println();
+            for (int j = 0; j < s; j++) {
+                for (int i = 0; i < len; i++) {
+                    second(s, number_list[i]);
+                    System.out.print(' ');
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < len; i++) {
+                bottom(s, number_list[i]);
+                System.out.print(' ');
+            }
+            System.out.println();
+            System.out.println();
+        }
+
+
+
+    }
+    public static void top(int s, char number) {
+        switch (number) {
+            case '1':
+            case '4':
+                top_print_false(s);
+                break;
+            default:
+                top_print_true(s);
+                break;
+        }
+    }
+    public static void middle(int s, char number) {
+        switch (number) {
+            case '1':
+            case '0':
+            case '7':
+                top_print_false(s);
+                break;
+            default:
+                top_print_true(s);
+                break;
+        }
+    }
+    public static void bottom(int s, char number) {
+        switch (number) {
+            case '1':
+            case '7':
+            case '4':
+                top_print_false(s);
+                break;
+            default:
+                top_print_true(s);
+                break;
+        }
+    }
+    public static void first (int s, char number) {
+        switch (number) {
+            case '1':
+            case '2':
+            case '3':
+            case '7':
+                three(s);
+                break;
+            case '5':
+            case '6':
+                one(s);
+                break;
+            default:
+                one_three(s);
+                break;
+        }
+    }
+    public static void second (int s, char number) {
+        switch (number) {
+            case '6':
+            case '8':
+            case '0':
+                one_three(s);
+                break;
+            case '2':
+                one(s);
+                break;
+            default:
+                three(s);
+                break;
+        }
+    }
+    public static void repeat_print_top (int num, char c) {
+        System.out.print(' ');
+        for (int i = 0; i < num; i++) {
+            System.out.print(c);
+        }
+        System.out.print(' ');
+    }
+    public static void top_print_true (int num){
+        repeat_print_top(num, '-');
+    }
+    public static void top_print_false (int num) {
+        repeat_print_top(num, ' ');
+    }
+    public static void one (int num) {
+        System.out.print('|');
+        for (int i = 0; i < num; i++) {
+            System.out.print(' ');
+        }
+        System.out.print(' ');
+    }
+    public static void three (int num) {
+        System.out.print(' ');
+        for (int i = 0; i < num; i++) {
+            System.out.print(' ');
+        }
+        System.out.print('|');
+    }
+    public static void one_three (int num) {
+        System.out.print('|');
+        for (int i = 0; i < num; i++) {
+            System.out.print(' ');
+        }
+        System.out.print('|');
     }
 }
