4f82351fcaae46acbcca8e359f7454ac14b158de
hamon1 <99328993+hamon1@users.noreply.github.com>
2023-01-18 21:39:41 +0900

    c17_try

--- Chapter03_String/WERTYU.java
+++ Chapter03_String/WERTYU.java
@@ -21,5 +21,19 @@ I AM FINE TODAY.
 
 package Chapter03_String;
 
+import java.util.Scanner;
+
 public class WERTYU {
+    static char[][] key = {{'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'},
+            {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'},
+            {'Z', 'X', 'C', 'V', 'B', 'N', 'M'}};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
+
+
+        }
+    }
 }
