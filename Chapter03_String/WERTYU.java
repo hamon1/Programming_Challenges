09754431fc5e2e816e33022d1f629a9b07097cae
hamon1 <99328993+hamon1@users.noreply.github.com>
2023-01-19 20:30:11 +0900

    c17_try

--- Chapter03_String/WERTYU.java
+++ Chapter03_String/WERTYU.java
@@ -21,19 +21,23 @@ I AM FINE TODAY.
 
 package Chapter03_String;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WERTYU {
-    static char[][] key = {{'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'},
-            {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'},
-            {'Z', 'X', 'C', 'V', 'B', 'N', 'M'}};
+    static char[][] key = {{'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'},
+            {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\''},
+            {'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'}};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Character> result = new ArrayList<>();
 
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
 
+            for (int i = 0; i < input.length(); i++) {
 
+            }
         }
     }
 }
